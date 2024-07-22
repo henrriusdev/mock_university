11c6c88bddcca9af4a23490399483bdee129847d
henrrius <henrrybrgt@gmail.com>
2024-07-21 22:17:59 -0400

    feat: updating entities

--- resources/js/lib/components/Navbar.svelte
+++ resources/js/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import {Menubar, Trigger, Content, Menu, Item, Separator, Sub, SubContent, SubTrigger} from '$lib/components/ui/menubar/index.js';
     export let careers = [];
 
 </script>
@@ -8,57 +8,57 @@
     <h1 class="text-2xl font-bold">
         <a href="/" class="">Mock University</a>
     </h1>
-    <Menubar.Root>
-        <Menubar.Menu>
-            <Menubar.Trigger>Careers</Menubar.Trigger>
-            <Menubar.Content>
+    <Menubar>
+        <Menu>
+            <Trigger>Careers</Trigger>
+            <Content>
                 {#if careers.length === 0}
-                    <Menubar.Item>No careers available</Menubar.Item>
+                    <Item>No careers available</Item>
                 {:else}
                     {#each careers as career}
-                        <Menubar.Item>{career.name}</Menubar.Item>
+                        <Item>{career.name}</Item>
                     {/each}
                 {/if}
-            </Menubar.Content>
-        </Menubar.Menu>
-        <Menubar.Menu>
-            <Menubar.Trigger>About</Menubar.Trigger>
-            <Menubar.Content>
-                <Menubar.Item>
+            </Content>
+        </Menu>
+        <Menu>
+            <Trigger>About</Trigger>
+            <Content>
+                <Item>
                     About <span class="text-bold">MockU</span>
-                </Menubar.Item>
-                <Menubar.Item>
+                </Item>
+                <Item>
                     Mission & Vision
-                </Menubar.Item>
-                <Menubar.Separator/>
-                <Menubar.Sub>
-                    <Menubar.SubTrigger>More about us</Menubar.SubTrigger>
-                    <Menubar.SubContent>
-                        <Menubar.Item>Our campuses</Menubar.Item>
-                        <Menubar.Separator/>
-                        <Menubar.Item>The Student Community</Menubar.Item>
-                        <Menubar.Item>The Teacher Community</Menubar.Item>
-                        <Menubar.Item>Admissions</Menubar.Item>
-                    </Menubar.SubContent>
-                </Menubar.Sub>
-            </Menubar.Content>
-        </Menubar.Menu>
-        <Menubar.Menu>
-            <Menubar.Trigger>Contact</Menubar.Trigger>
-            <Menubar.Content>
-                <Menubar.Item inset>WhatsApp</Menubar.Item>
-                <Menubar.Separator/>
-                <Menubar.Item inset>Facebook</Menubar.Item>
-                <Menubar.Item inset>Twitter</Menubar.Item>
-                <Menubar.Item inset>Instagram</Menubar.Item>
-                <Menubar.Separator/>
-                <Menubar.Item inset>Contact page</Menubar.Item>
-            </Menubar.Content>
-        </Menubar.Menu>
-        <Menubar.Menu>
-            <Menubar.Trigger class="bg-default">
+                </Item>
+                <Separator/>
+                <Sub>
+                    <SubTrigger>More about us</SubTrigger>
+                    <SubContent>
+                        <Item>Our campuses</Item>
+                        <Separator/>
+                        <Item>The Student Community</Item>
+                        <Item>The Teacher Community</Item>
+                        <Item>Admissions</Item>
+                    </SubContent>
+                </Sub>
+            </Content>
+        </Menu>
+        <Menu>
+            <Trigger>Contact</Trigger>
+            <Content>
+                <Item inset>WhatsApp</Item>
+                <Separator/>
+                <Item inset>Facebook</Item>
+                <Item inset>Twitter</Item>
+                <Item inset>Instagram</Item>
+                <Separator/>
+                <Item inset>Contact page</Item>
+            </Content>
+        </Menu>
+        <Menu>
+            <Trigger class="bg-default">
                 <a href="/login" class="text-default-foreground">Login</a>
-            </Menubar.Trigger>
-        </Menubar.Menu>
-    </Menubar.Root>
+            </Trigger>
+        </Menu>
+    </Menubar>
 </div>
