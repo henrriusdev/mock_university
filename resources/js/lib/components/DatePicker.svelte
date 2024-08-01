c07cf863a03e1b89a71573662662a0ea8fbdc746
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-08-01 00:00:12 -0400

    chore: Update DatePicker component formatting

--- resources/js/lib/components/DatePicker.svelte
+++ resources/js/lib/components/DatePicker.svelte
@@ -1,37 +1,33 @@
 <script>
-    import CalendarIcon from "lucide-svelte/icons/calendar";
-    import {
-        DateFormatter,
-        getLocalTimeZone
+  import CalendarIcon from "lucide-svelte/icons/calendar";
+  import { DateFormatter, getLocalTimeZone } from "@internationalized/date";
+  import { cn } from "$lib/utils.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Calendar } from "$lib/components/ui/calendar/index.js";
+  import * as Popover from "$lib/components/ui/popover/index.js";
 
-    } from "@internationalized/date";
-    import { cn } from "$lib/utils.js";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import { Calendar } from "$lib/components/ui/calendar/index.js";
-    import * as Popover from "$lib/components/ui/popover/index.js";
+  const df = new DateFormatter("en-US", {
+    dateStyle: "long",
+  });
 
-    const df = new DateFormatter("en-US", {
-        dateStyle: "long"
-    });
-
-    export let value = undefined;
+  export let value = undefined;
 </script>
 
 <Popover.Root>
-    <Popover.Trigger asChild let:builder>
-        <Button
-                variant="outline"
-                class={cn(
-        "w-[280px] justify-start text-left font-normal",
-        !value && "text-muted-foreground"
-      )}
-                builders={[builder]}
-        >
-            <CalendarIcon class="mr-2 h-4 w-4" />
-            {value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date"}
-        </Button>
-    </Popover.Trigger>
-    <Popover.Content class="w-auto p-0">
-        <Calendar bind:value initialFocus />
-    </Popover.Content>
+  <Popover.Trigger asChild let:builder>
+    <Button
+      variant="outline"
+      class="{cn(
+        'w-[280px] justify-start text-left font-normal',
+        !value && 'text-muted-foreground'
+      )}"
+      builders="{[builder]}"
+    >
+      <CalendarIcon class="mr-2 h-4 w-4" />
+      {value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date"}
+    </Button>
+  </Popover.Trigger>
+  <Popover.Content class="w-auto p-0">
+    <Calendar bind:value initialFocus />
+  </Popover.Content>
 </Popover.Root>
