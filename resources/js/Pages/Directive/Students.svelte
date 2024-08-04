39f78519d073566870a6501f2e6067b562380d6c
Henrrius <henrrybrgt@gmail.com>
2024-08-03 22:06:53 -0400

    feat: adding data-table
    Adding custom and editable data table for components, is pretty, and new
    view called Students at Pages/Directive folder

---
+++ resources/js/Pages/Directive/Students.svelte
@@ -0,0 +1,46 @@
+<script>
+  import DataTable from "$lib/datatable/data-table.svelte";
+  import { createTable } from "svelte-headless-table";
+  import { readable } from "svelte/store";
+
+  /** @type { Array<{id: number, avatar: string, name: string, email: string, phone: string, career: string, totalAverage: number}>} */
+  export let students = [];
+
+  const table = createTable(readable(students));
+  const columns = table.createColumns([
+    table.column({
+      accessor: "id",
+      header: "ID",
+    }),
+    table.column({
+      accessor: "avatar",
+      header: " ",
+    }),
+    table.column({
+      accessor: "name",
+      header: "Name",
+    }),
+    table.column({
+      accessor: "email",
+      header: "Email",
+    }),
+    table.column({
+      accessor: "phone",
+      header: "Phone",
+    }),
+    table.column({
+      accessor: "career",
+      header: "Career",
+    }),
+    table.column({
+      accessor: "totalAverage",
+      header: "Total Average",
+    }),
+    table.column({
+      accessor: ({ id }) => id,
+      header: "Actions",
+    }),
+  ]);
+</script>
+
+<DataTable {table} {columns} />
---
+++ resources/js/lib/datatable/data-table.svelte
@@ -0,0 +1,60 @@
+<script>
+  import * as Table from "$lib/components/ui/table/index";
+  import DirectiveLayout from "$lib/layouts/DirectiveLayout.svelte";
+  import { Render, Subscribe } from "svelte-headless-table";
+
+  /**
+   * @typedef {import('svelte-headless-table').Table<any, any>} DataTable
+   * @typedef {import('svelte-headless-table').Column<any, any>} DataTableColumn
+   */
+
+  /** @type {DataTableColumn[]} */
+  export let columns;
+
+  /** @type {DataTable} */
+  export let table;
+
+  const { headerRows, pageRows, tableAttrs, tableBodyAttrs } =
+    table.createViewModel(columns);
+</script>
+
+<DirectiveLayout
+  title="Students List | Directive | Mock University"
+  description="Students List of the Mock University."
+  keywords="students, list"
+>
+  <div class="rounded-md border">
+    <Table.Root {...$tableAttrs}>
+      <Table.Header>
+        {#each $headerRows as headerRow}
+          <Subscribe rowAttrs={headerRow.attrs()}>
+            <Table.Row>
+              {#each headerRow.cells as cell (cell.id)}
+                <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
+                  <Table.Head {...attrs}>
+                    <Render of={cell.render()} />
+                  </Table.Head>
+                </Subscribe>
+              {/each}
+            </Table.Row>
+          </Subscribe>
+        {/each}
+      </Table.Header>
+      <Table.Body {...$tableBodyAttrs}>
+        {#each $pageRows as row (row.id)}
+          <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+            <Table.Row {...rowAttrs}>
+              {#each row.cells as cell (cell.id)}
+                <Subscribe attrs={cell.attrs()} let:attrs>
+                  <Table.Cell {...attrs}>
+                    <Render of={cell.render()} />
+                  </Table.Cell>
+                </Subscribe>
+              {/each}
+            </Table.Row>
+          </Subscribe>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
+</DirectiveLayout>
