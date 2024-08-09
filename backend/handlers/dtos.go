813cbc093aa483b91f37e2a36fc89caa83ae1cc9
henrrius.dev <henrrybrgt@gmail.com>
2024-08-09 12:36:03 -0400

    feat: Update StudentDto and StudentsTableDto in backend/handlers/dtos.go
    This commit updates the `StudentDto` and `StudentsTableDto` structs in the `backend/handlers/dtos.go` file. It adds new fields and modifies existing ones to better represent the data for students and the students table.

--- backend/handlers/dtos.go
+++ backend/handlers/dtos.go
@@ -5,12 +5,35 @@ type LoginDto struct {
 	Password string `json:"password"`
 }
 
-type StudentDtoI struct {
+type StudentDto struct {
+	ID                     int     `json:"id"`
+	IdentityCard           string  `json:"identityCard"`
+	BirthDate              string  `json:"birthDate"`
+	Phone                  string  `json:"phone"`
+	Address                string  `json:"address"`
+	District               string  `json:"district"`
+	City                   string  `json:"city"`
+	PostalCode             int     `json:"postalCode"`
+	CreditUnitsAccumulated int     `json:"creditUnitsAccumulated"`
+	TotalAverage           float64 `json:"totalAverage"`
+}
+
+type StudentsTableDto struct {
 	ID           int     `json:"id"`
-	Name         string  `json:"name"`
+	IdentityCard string  `json:"identityCard"`
 	Avatar       string  `json:"avatar"`
+	Name         string  `json:"name"`
 	Email        string  `json:"email"`
 	Phone        string  `json:"phone"`
 	Career       string  `json:"career"`
-	TotalAverage float64 `json:"total_average"`
+	TotalAverage float64 `json:"totalAverage"`
+}
+
+type UserDto struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Avatar   string `json:"avatar"`
+	Active   bool   `json:"active"`
 }
