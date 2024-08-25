ed30dfe78082fa0b361f8dadba3c64ad15b4f141
henrriusdev <henrrybrgt@gmail.com>
2024-08-25 13:20:18 -0400

    refactor: Update JavaScript code to use minified versions of dependencies

--- ssr.config.js
+++ ssr.config.js
@@ -2,33 +2,4 @@ import { defineConfig } from 'vite';
 import {svelte} from "@sveltejs/vite-plugin-svelte"
 import laravel from "laravel-vite-plugin";
 import path from "path";
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.js', 'resources/css/app.css'],
-            ssr: 'resources/js/ssr.js', // Enable SSR
-            publicDirectory: 'public',
-            buildDirectory: 'bootstrap',
-            refresh: ['resources/**'],
-        }),
-        svelte(),
-    ],
-    build: {
-        ssr: true, // Enable SSR
-        outDir: 'bootstrap',
-        rollupOptions: {
-            input: 'resources/js/ssr.js',
-            output: {
-                entryFileNames: 'assets/[name].js',
-                chunkFileNames: 'assets/[name].js',
-                assetFileNames: 'assets/[name][extname]',
-                manualChunks: undefined, // Disable automatic chunk splitting
-            },
-        },
-    },
-    resolve: {
-        alias: {
-            $lib: path.resolve(__dirname, 'resources/js/lib'),
-        },
-    },
-});
+export default defineConfig({plugins: [laravel({input: ['resources/js/app.js', 'resources/css/app.css'],ssr: 'resources/js/ssr.js', publicDirectory: 'public',buildDirectory: 'bootstrap',refresh: ['resources/**'],}),svelte(),],build: {ssr: true, outDir: 'bootstrap',rollupOptions: {input: 'resources/js/ssr.js',output: {entryFileNames: 'assets/[name].js',chunkFileNames: 'assets/[name].js',assetFileNames: 'assets/[name][extname]',manualChunks: undefined, },},},resolve: {alias: {$lib: path.resolve(__dirname, 'resources/js/lib'),},}})
--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,4 @@ import laravel from 'laravel-vite-plugin';
 import {svelte} from "@sveltejs/vite-plugin-svelte"
 import path from "path";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            publicDirectory: 'public',
-            buildDirectory: 'build',
-            refresh: ['resources/**'],
-        }),
-        svelte({
-            compilerOptions: {
-                hydratable: true,
-            },
-        }),
-    ],
-    build: {
-        manifest: true, // Generate manifest.json file
-        outDir: 'public/build',
-        rollupOptions: {
-            input: 'resources/js/app.js',
-            output: {
-                entryFileNames: 'assets/[name].js',
-                chunkFileNames: 'assets/[name].js',
-                assetFileNames: 'assets/[name].[ext]',
-                manualChunks: undefined, // Disable automatic chunk splitting
-            },
-        },
-    },
-    resolve: {
-        alias: {
-            $lib: path.resolve(__dirname, 'resources/js/lib'),
-        },
-    },
-});
+export default defineConfig({plugins: [laravel({input: 'resources/js/app.js',publicDirectory: 'public',buildDirectory: 'build',refresh: ['resources/**'],}),svelte({compilerOptions: {hydratable: true,},}),],build: {manifest: true, outDir: 'public/build',rollupOptions: {input: 'resources/js/app.js',output: {entryFileNames: 'assets/[name].js',chunkFileNames: 'assets/[name].js',assetFileNames: 'assets/[name].[ext]',manualChunks: undefined, },},},resolve: {alias: {$lib: path.resolve(__dirname, 'resources/js/lib'),},}});
